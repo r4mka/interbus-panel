eb1f51e8eb856853f8c3f9d85033f1e59f451a17
Adam Ramski <adam.ramski@meelogic.com>
2019-07-18 21:23:20 +0200

    install styled-components and facepaint

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
 export { default as initializeApiClient } from './apiClient';
 export { default as PropTypes } from './propTypes';
+export { default as mq } from './mediaqueries';
 
 export const toArray = (value = []) => (typeof value === 'string' ? [value] : value);
